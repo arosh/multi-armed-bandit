53209bbfa9e2badd8ba45186b7ff140d61f51102
Sho Iizuka <sho.i518@gmail.com>
2017-02-19 19:20:21 +0900

    スロットマシーンの画像とボタンを追加

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import slot from './money_slot_machine.png';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const styles = {
+  slot: {
+    marginBottom: '1em',
+  },
+};
+
+const App = () => (
+  <div className="container">
+    <div className="col-xs-offset-2 col-xs-3">
+      <img src={slot} alt="スロットマシーン" className="img-responsive" style={styles.slot} />
+      <button type="button" className="btn btn-primary btn-lg btn-block">レバーを引く</button>
+    </div>
+    <div className="col-xs-offset-2 col-xs-3">
+      <img src={slot} alt="スロットマシーン" className="img-responsive" style={styles.slot} />
+      <button type="button" className="btn btn-primary btn-lg btn-block">レバーを引く</button>
+    </div>
+  </div>
+);
 
 export default App;
